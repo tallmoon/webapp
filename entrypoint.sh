39670c654a80bf59d564ed89f37c96e2f701863a
tallmoon <88712902+tallmoon@users.noreply.github.com>
2023-08-11 22:17:56 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID='de04add9-5c68-8bab-950c-08cd5320df18'
-VMESS_WSPATH='/vmess'
-VLESS_WSPATH='/vless'
+UUID='730cf919-3d9d-4cee-8faa-8443f219fa9e'
+VMESS_WSPATH='/vmee3rfdss45ts'
+VLESS_WSPATH='/vlew345tnbvfdss'
 TROJAN_WSPATH='/trojan'
 SS_WSPATH='/shadowsocks'
 sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#TROJAN_WSPATH#${TROJAN_WSPATH}#g;s#SS_WSPATH#${SS_WSPATH}#g" config.json
